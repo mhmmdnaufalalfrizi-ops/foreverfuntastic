fc6e395460257b843d652a946654a5eff58064ce
mhmmdnaufalalfrizi-ops <mhmmdnaufalalfrizi@gmail.com>
2026-08-30 11:40:28 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,6 @@ const GALLERY_PHOTOS = [
    { file: "8.jpg", caption: "Ganti dengan captionmu" },
    { file: "9.jpg", caption: "Ganti dengan captionmu" },
    { file: "10.jpg", caption: "Ganti dengan captionmu" },
-   { file: "11.jpg", caption: "" },
 ];
 const GALLERY_PATH = "assets/img/gallery/";
 const GALLERY_PLACEHOLDER_COUNT = 18; // jumlah kotak placeholder kalau galeri masih kosong
